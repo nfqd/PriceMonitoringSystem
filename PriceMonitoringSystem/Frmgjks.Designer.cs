7d730d0dd4048ce1b2d77628e5b8cbbd4648f7d9
nfqd <616989840@qq.com>
2018-12-19 18:46:28 +0800

    Create Frmgjks.Designer.cs

---
+++ PriceMonitoringSystem/Frmgjks.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace PriceMonitoringSystem
+{
+    partial class Frmgjks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ttcode = new System.Windows.Forms.TextBox();
+            this.ttvalue = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tttype = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(305, 25);
+            this.panel1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "国家代码：";
+            // 
+            // ttcode
+            // 
+            this.ttcode.Location = new System.Drawing.Point(84, 44);
+            this.ttcode.Name = "ttcode";
+            this.ttcode.Size = new System.Drawing.Size(177, 21);
+            this.ttcode.TabIndex = 5;
+            // 
+            // ttvalue
+            // 
+            this.ttvalue.Location = new System.Drawing.Point(84, 82);
+            this.ttvalue.Name = "ttvalue";
+            this.ttvalue.Size = new System.Drawing.Size(177, 21);
+            this.ttvalue.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "名    称：";
+            // 
+            // tttype
+            // 
+            this.tttype.Location = new System.Drawing.Point(84, 123);
+            this.tttype.Name = "tttype";
+            this.tttype.Size = new System.Drawing.Size(177, 21);
+            this.tttype.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "类    型：";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnAdd.Location = new System.Drawing.Point(0, 0);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(65, 25);
+            this.btnAdd.TabIndex = 14;
+            this.btnAdd.Text = "保存";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // FrmTen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 188);
+            this.Controls.Add(this.tttype);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ttvalue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ttcode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmTen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "编辑数据";
+            this.Load += new System.EventHandler(this.FrmTen_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ttcode;
+        private System.Windows.Forms.TextBox ttvalue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tttype;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
